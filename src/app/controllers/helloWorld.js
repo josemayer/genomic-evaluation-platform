8112c38bfe4a97ef6d5b62a3cde5bd562341bb78
José Lucas Silva Mayer <josemayer@usp.br>
2023-06-06 03:08:27 -0400

    Add OK status codes and return responses
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/app/controllers/helloWorld.js
+++ src/app/controllers/helloWorld.js
@@ -2,25 +2,34 @@ const env = require('../config/env');
 const helloWorld = require('../services/helloWorld');
 
 function sayHello(req, res, next) {
+  res.status(200);
   res.json({ message: 'Hello, World!' });
+  return res;
 }
 
 function sayHelloWithEnv(req, res, next) {
+  res.status(200);
   res.json({ message: `Hello, World! We're on version ${env.app.version} of ${env.app.name}!` });
+  return res;
 }
 
 function sayHelloWithName(req, res, next) {
   const { name } = req.params;
+  res.status(200);
   res.json({ message: `Hello, ${name}!` });
+  return res;
 }
 
 async function sayHelloToAllInPostgres(req, res, next) {
   try {
     const helloWorlds = await helloWorld.getAll();
+    res.status(200);
     res.json({ messages: helloWorlds });
   } catch (err) {
-    res.status(err.statusCode || 500).json({ message: err.message });
+    res.status(err.statusCode || 500);
+    res.json({ message: err.message });
   }
+  return res;
 }
 
 module.exports = {
