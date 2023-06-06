2bb2ce00dde014f6b2b8147b8e7e23d9b4962e70
José Lucas Silva Mayer <josemayer@usp.br>
2023-06-06 19:18:04 -0400

    Add Luã to database
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/dbs/postgres/schema/1-helloWorld.sql
+++ src/dbs/postgres/schema/1-helloWorld.sql
@@ -2,6 +2,6 @@ CREATE TABLE hello_world (
   name VARCHAR(50) NOT NULL PRIMARY KEY
 );
 
-INSERT INTO hello_world (name) VALUES ('Antônio');
+INSERT INTO hello_world (name) VALUES ('Luã');
 INSERT INTO hello_world (name) VALUES ('Zé');
 INSERT INTO hello_world (name) VALUES ('Max');
