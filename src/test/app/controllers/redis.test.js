a1583f338e0042f91d7471a3aabf3bf35425d4aa
Luã Santilli <lua.til@usp.br>
2023-06-08 20:15:05 -0300

    redis: Implement basic tests for the redis controller
    Signed-off-by: Luã Santilli <lua.til@usp.br>

---
+++ src/test/app/controllers/redis.test.js
@@ -0,0 +1,42 @@
+const redis = require('../../../app/controllers/redis');
+
+describe("redis controller", () => {
+  let mockRequest;
+  let mockResponse;
+  let mockNext;
+
+  beforeEach(() => {
+    mockRequest = {};
+    mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockReturnThis(),
+    };
+    mockNext = jest.fn();
+  });
+
+  describe("with passing", () => {
+    describe("setKeyWithValue", () => {
+      it("should return Setting key:value", async () => {
+        mockRequest.params = { key: 'a', value: 'b' };
+
+        await redis.setKeyWithValue(mockRequest, mockResponse, mockNext);
+
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+        expect(mockResponse.json).toHaveBeenCalledWith({ message: 'Setting a:b' });
+        expect(mockNext).not.toHaveBeenCalled();
+      });
+    });
+
+    describe("getKey", () => {
+      it("should return b", async () => {
+        mockRequest.params = { key: 'a'};
+
+        await redis.getValue(mockRequest, mockResponse, mockNext);
+
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+        expect(mockResponse.json).toHaveBeenCalledWith({ message: 'b' });
+        expect(mockNext).not.toHaveBeenCalled();
+      });
+    });
+  });
+})
