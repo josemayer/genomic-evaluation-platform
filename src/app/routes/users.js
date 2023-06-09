6a15e4e9b2ff511163d1ffa9d6b00c2013f62ef3
José Lucas Silva Mayer <josemayer@usp.br>
2023-06-09 04:28:14 -0400

    Add client info with id subroute to users route
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/app/routes/users.js
+++ src/app/routes/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usersController = require('../controllers/users');
 
 router.get('/clients', usersController.listClients);
+router.get('/client/:id', usersController.clientInfo);
 
 module.exports = router;
