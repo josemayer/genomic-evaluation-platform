845e09a2b73e32d4c9fae6a25166ae71881c1996
José Lucas Silva Mayer <josemayer@usp.br>
2023-06-09 05:13:07 -0400

    Add new subroute to register a new client in users route
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/app/routes/users.js
+++ src/app/routes/users.js
@@ -4,5 +4,6 @@ const usersController = require('../controllers/users');
 
 router.get('/clients', usersController.listClients);
 router.get('/client/:id', usersController.clientInfo);
+router.post('/client/new', usersController.registerClient);
 
 module.exports = router;
