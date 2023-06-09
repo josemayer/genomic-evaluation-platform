40f4bbb7a05fe9077d8cca8679e9912b552b1283
José Lucas Silva Mayer <josemayer@usp.br>
2023-06-09 02:24:27 -0400

    Add method getClientById in user service with tests
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/app/services/users.js
+++ src/app/services/users.js
@@ -17,6 +17,25 @@ async function getAllClients() {
   return clientsInfo;
 }
 
+async function getClientById(id) {
+  const client = await pg.query(
+    "SELECT u.id, u.nome_completo, u.email, c.telefone FROM usuario AS u, cliente AS c WHERE u.id = c.usuario_id AND u.id = $1",
+    [id]
+  );
+
+  if (client.rows.length > 0) {
+    const clientElement = client.rows[0];
+    const clientInfo = {
+      name: clientElement.nome_completo,
+      mail: clientElement.email,
+      phone: clientElement.telefone,
+    };
+    return clientInfo;
+  }
+  return null;
+}
+
 module.exports = {
   getAllClients,
+  getClientById,
 };
--- src/test/app/services/users.test.js
+++ src/test/app/services/users.test.js
@@ -56,6 +56,40 @@ describe('users service', () => {
         );
       });
     });
+
+    describe('getClientById', () => {
+      it.each([
+        [
+          [
+            {
+              id: 1,
+              nome_completo: 'Alice',
+              email: 'alice@exemplo.com',
+              telefone: '(11) 99999-9999',
+            }
+          ],
+          {
+            name: 'Alice',
+            mail: 'alice@exemplo.com',
+            phone: '(11) 99999-9999'
+          }
+        ],
+        [
+          [],
+          null
+        ],
+      ])('should return an object with found client info', async (mockRow, expectedResult) => {
+        pg.query.mockResolvedValue({ rows: mockRow });
+
+        const result = await users.getClientById(1);
+
+        expect(result).toEqual(expectedResult);
+        expect(pg.query).toHaveBeenCalledWith(
+          "SELECT u.id, u.nome_completo, u.email, c.telefone FROM usuario AS u, cliente AS c WHERE u.id = c.usuario_id AND u.id = $1",
+          [1]
+        );
+      });
+    });
   });
 
   describe('with failing', () => {
