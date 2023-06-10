eb76b37c220d96daf8e2a0e0cb924adf6d00b247
Luã Santilli <lua.til@usp.br>
2023-06-10 00:48:22 -0300

    redis: Implement addCondition controller and route
    Signed-off-by: Luã Santilli <lua.til@usp.br>

--- src/app/controllers/redis.js
+++ src/app/controllers/redis.js
@@ -1,4 +1,37 @@
+// @ts-check
 const redis = require('../services/redis');
+const express = require('express');
+
+/**
+  * @param {express.Request<{id: string, sequences: string}>} req
+  * @param {express.Response} res
+  */
+async function addCondition(req, res) {
+
+  const { id, sequences } = req.params;
+
+  const num = parseInt(id);
+
+  if (isNaN(num)) {
+    res.status(400);
+    res.json({ message: `${id} should be a number - the condition id` });
+    return res;
+  }
+
+  /** @type {Map<string, number>} */
+  const sequencesWithProbs = new Map();
+
+  // TODO(luatil): Do validation here
+  sequences.split("-").forEach((el) => {
+    const [seq, prob] = el.split(":");
+    sequencesWithProbs.set(seq, parseFloat(prob));
+  });
+
+  res.status(200);
+  const redisServiceResult = await redis.addCondition(num, sequencesWithProbs);
+  res.json({ message: redisServiceResult });
+  return res;
+}
 
 async function helloWorldFromRedis(req, res, next) {
   res.status(200);
@@ -30,5 +63,6 @@ async function addUser(req, res, next) {
 module.exports = {
   setKeyWithValue,
   getValue,
-  helloWorldFromRedis
+  helloWorldFromRedis,
+  addCondition,
 }
--- src/app/routes/redis.js
+++ src/app/routes/redis.js
@@ -5,5 +5,6 @@ const redisController = require('../controllers/redis');
 router.get('/get/:key', redisController.getValue);
 router.get('/set/:key/:value', redisController.setKeyWithValue);
 router.get('/helloWorld', redisController.helloWorldFromRedis);
+router.get('/addCondition/:id/:sequences', redisController.addCondition);
 
 module.exports = router;
