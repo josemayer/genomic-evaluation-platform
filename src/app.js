0de4fab3d5d05630fd72fb025743d90b3d403342
José Lucas Silva Mayer <josemayer@usp.br>
2023-06-10 18:30:19 -0400

    Add exams route to app
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const env = require('./app/config/env');
 
 const indexRoute = require('./app/routes/index');
 const helloWorldRoute = require('./app/routes/helloWorld');
+const examsRoute = require('./app/routes/exams');
 
 const app = express();
 
 app.use('/', indexRoute);
 app.use('/helloWorld', helloWorldRoute);
+app.use('/exams', examsRoute);
 
 const network = env.network;
 const appInfo = env.app;
