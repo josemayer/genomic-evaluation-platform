bd42f763fe01279c601ea17d15eb0def3f8b288b
Luã Santilli <lua.til@usp.br>
2023-06-10 00:47:34 -0300

    redis: Implement addCondition service
    Signed-off-by: Luã Santilli <lua.til@usp.br>

--- src/app/services/redis.js
+++ src/app/services/redis.js
@@ -1,5 +1,22 @@
 const redis = require('../config/redis');
 
+/** 
+ * @param {number} id
+ * @param {Map<string, number>} sequenceWithProbs
+ */
+async function addCondition(id, sequencesWithProbs) {
+  // TODO(luatil): Deal with failures
+  const sadd = ['sadd', `condition:${id}:sequence`,
+    Array.from(sequencesWithProbs.keys()).join(" ")]
+  const saddResult = await redis.execute(sadd);
+
+  const hset = ['hset', `condition:${id}:probs`]
+    .concat(Array.from(sequencesWithProbs.entries()).flat());
+  const hsetResult = await redis.execute(hset);
+
+  return [saddResult, hsetResult];
+}
+
 async function helloWorld() {
   const result = await redis.helloWorld();
   const set = new Set(result.map((el) => el.name).filter((el) => el));
@@ -21,4 +38,5 @@ module.exports = {
   set,
   get,
   helloWorld,
+  addCondition
 };
