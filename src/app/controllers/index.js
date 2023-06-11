83932426c999a2d75dcadf4da9a3f2cd73d88fe7
José Lucas Silva Mayer <josemayer@usp.br>
2023-06-11 12:19:48 -0400

    Remove Hello World route informations
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -4,26 +4,6 @@ function showRoutes(req, res, next) {
     routes: [
       {
         method: 'GET',
-        path: '/helloWorld',
-        description: 'Returns a simple hello-world message.',
-      },
-      {
-        method: 'GET',
-        path: '/helloWorld/env',
-        description: 'Returns a simple hello-world message with environment config variables.',
-      },
-      {
-        method: 'GET',
-        path: '/helloWorld/name/{any name}',
-        description: 'Returns a simple hello-world message with the name provided.',
-      },
-      {
-        method: 'GET',
-        path: '/helloWorld/postgres',
-        description: 'Returns a simple hello-world message with the names retrieved from a Postgres database.',
-      },
-      {
-        method: 'GET',
         path: '/users/clients',
         description: 'Returns a list of clients from the database.',
       },
