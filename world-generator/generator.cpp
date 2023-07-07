bfdf9139cb0b69347b80b4596e6132399dec2fdf
Maximilian CG <mcgoeritz@gmail.com>
2023-07-07 19:39:20 -0300

    Implement a person generator

--- world-generator/generator.cpp
+++ world-generator/generator.cpp
@@ -4,21 +4,53 @@
 #include <string>
 #include <deque>
 #include <map>
+#include <set>
 #include <cstdlib>
 #include <time.h>
 
-using std::string;
-using argList = std::deque<string>;
 #define fs std::filesystem
 
-#define WORLD string("./world/")
+using std::string;
+using argList = std::deque<string>;
+using path = std::filesystem::path;
+
+#define WORLD path("./world")
+
+#define DNA_SIZE 1000
+
+#define CONDITION_CHANCE 100
+#define CONDITION_THRESHOLD RAND_MAX / CONDITION_CHANCE
 
 std::map<string, int(*)(argList)> commandMap;
 
-void assert_directory_exists(string path) {
-	if (!fs::exists(path)) {
-		fs::create_directory(path);
+void assertDirectoryExists(path p) {
+	if (!fs::exists(p)) {
+		fs::create_directory(p);
 	}
+}
+
+std::ofstream writeToWorld(string subdir, string file) {
+	assertDirectoryExists(WORLD / subdir);
+	return std::ofstream(WORLD / subdir / file);
+}
+
+std::ifstream readFromWorld(string subdir, string file) {
+	return std::ifstream(WORLD / subdir / file);
+}
+
+std::set<int> getConditionGenes() {
+	std::set<int> genes;
+	for (auto const& cond_file : fs::directory_iterator(WORLD / "conditions")) {
+		std::ifstream file(cond_file.path());
+		int geneCount;
+		file>>geneCount;
+		while (geneCount--) {
+			int gene;
+			file>>gene;
+			genes.insert(gene);
+		}
+	}
+	return genes;
 }
 
 int help(argList args) {
@@ -32,45 +64,81 @@ int help(argList args) {
 }
 
 int newCondition(argList args) {
+	if (args.size() < 1) {
+		std::cout<<"Missing condition name"<<std::endl;
+		return 1;
+	}
+
 	if (args[0] == "help") {
 		return 0;
 	}
 
-	string path = WORLD + "conditions/";
+	int x;
+	int min = 10;
+	std::set<int> genes;
+	do {
+		x = rand();
+		genes.insert(x ^ -1);
+	} while (x <= RAND_MAX / ((genes.size() + min - 1) / min));
 
-	assert_directory_exists(path);
+	std::ofstream file = writeToWorld("conditions", args[0]);
 
-	path += args[0];
-	if (fs::exists(path)) {
-		std::cerr<<"Condition with the same name already exists"<<std::endl;
+	file << genes.size();
+	for (auto gene : genes) {
+		file << gene << std::endl;
+	}
+
+	return 0;
+}
+
+int newPerson(argList args) {
+	if (args.size() < 1) {
+		std::cout<<"Missing person name";
 		return 1;
 	}
 
-	std::cout<<"writing to "<<path<<std::endl;
-	std::ofstream file(path);
+	if (args[0] == "help") {
+		return 0;
+	}
 
 	int x;
-	int i = 0;
-	int min = 10;
-	do {
-		x = rand();
-		file << (x ^ -1) << std::endl;
-		i++;
-	} while (x <= RAND_MAX / ((i + min - 1) / min));
+	std::set<int> genes;
+	std::set<int> condGenes = getConditionGenes();
+
+	while (genes.size() < DNA_SIZE) {
+		if (rand() < CONDITION_THRESHOLD) {
+			int x = rand() % condGenes.size();
+			auto it = condGenes.begin();
+			for (int i = 0; i < x; i++) {
+				it++;
+			}
+
+			genes.insert(*it);
+		} else {
+			genes.insert(rand());
+		}
+	}
+
+	auto f = writeToWorld("people", args[0]);
+
+	for (auto g : genes) {
+		f<<g<<std::endl;
+	}
 
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
+	commandMap = {
+		{"new_condition", newCondition},
+		{"new_person", newPerson},
+		{"help", help},
+	};
+
 	if (argc < 2) {
 		help({});
 		return 1;
 	}
-
-	commandMap = {
-		{"new_condition", newCondition},
-		{"help", help},
-	};
 
 	srand(time(NULL));
 
