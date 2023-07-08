e3fedde85afce7d3674c3a01a2defd21a99d622e
Luã Santilli <luasantilli@gmail.com>
2023-07-08 17:18:12 -0300

    cli: Implement sample login system for the CLI
    Signed-off-by: Luã Santilli <luasantilli@gmail.com>

--- cli/main.py
+++ cli/main.py
@@ -1,40 +1,70 @@
+import http.client
+import json
+
+global_token = None
+
+
+def get_token(mail, password):
+    conn = http.client.HTTPConnection("localhost", 4000)
+    payload = json.dumps({
+        "mail": mail,
+        "password": password
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    conn.request("POST", "/users/login", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    data = data.decode("utf-8")
+    data = json.loads(data)
+    if "token" not in data:
+        return False
+    return data['token']
+
+
 # returns a token
 def login(email, password):
-    if email == "lua.til" and password == "123":
-        # => Enviar o email e senha para o servidor
-        # <= Receber o token do servidor
-        print("Login bem sucedido")
-        return hash(email+password)
-    elif email == "lua.til" and password != "123":
+
+    global global_token
+
+    token_or_false = get_token(email, password)
+
+    if not token_or_false:
         print("Senha incorreta")
         return False
-    else:
-        print("Login incorreto")
-        return False
+
+    print("Login bem sucedido")
+    global_token = token_or_false
+    print(token_or_false)
+    return True
+
 
 def notifications():
     notifications = ["Voce tem que fazer o teste do joao", "Voce tem que fazer o teste do pedro"]
     for noti in notifications:
         print(noti)
 
+
 def verifiy_exam(exam_id):
     print(f"Verificando o exame {exam_id}")
     summary = "Identifiquei:"
 
     conditions = [
-            ["chule", "0.8"],
-            ["asma", "0.1"],
-            ["astigmatismo", "0.7"],
-            ["cancer_de_pulmao", "0.001"]
-        ]
-    
-    relations = [
-            ["Pai", "Joao"],
-            ["Mae", "Maria"],
-            ["Filho", "Joao Vinicius"],
-        ]
+        ["chule", "0.8"],
+        ["asma", "0.1"],
+        ["astigmatismo", "0.7"],
+        ["cancer_de_pulmao", "0.001"]
+    ]
 
-    res = summary + '\nCONDICOES\n' + "\n".join([": ".join(cond) for cond in conditions]) + "\nRELACOES\n" + "\n".join([": ".join(rel) for rel in relations])
+    relations = [
+        ["Pai", "Joao"],
+        ["Mae", "Maria"],
+        ["Filho", "Joao Vinicius"],
+    ]
+
+    res = summary + '\nCONDICOES\n' + "\n".join([": ".join(cond) for cond in conditions]) + "\nRELACOES\n" + "\n".join(
+        [": ".join(rel) for rel in relations])
     print(res)
     answer = input("Y/N: ")
 
@@ -47,40 +77,36 @@ def verifiy_exam(exam_id):
         print("Exame rejeitado")
 
 
-
 def do_exam(exam_id):
     print("Fazendo o exame {}".format(exam_id))
     summary = "Identifiquei:"
 
     conditions = [
-            ["chule", "0.8"],
-            ["asma", "0.1"],
-            ["astigmatismo", "0.7"],
-            ["cancer_de_pulmao", "0.001"]
-        ]
-    
-    relations = [
-            ["Pai", "Joao"],
-            ["Mae", "Maria"],
-            ["Filho", "Joao Vinicius"],
-        ]
+        ["chule", "0.8"],
+        ["asma", "0.1"],
+        ["astigmatismo", "0.7"],
+        ["cancer_de_pulmao", "0.001"]
+    ]
 
-    res = summary + '\nCONDICOES\n' + "\n".join([": ".join(cond) for cond in conditions]) + "\nRELACOES\n" + "\n".join([": ".join(rel) for rel in relations])
+    relations = [
+        ["Pai", "Joao"],
+        ["Mae", "Maria"],
+        ["Filho", "Joao Vinicius"],
+    ]
+
+    res = summary + '\nCONDICOES\n' + "\n".join([": ".join(cond) for cond in conditions]) + "\nRELACOES\n" + "\n".join(
+        [": ".join(rel) for rel in relations])
     print(res)
 
 
 def main():
-
     logged = False
-    tok = None
 
     while not logged:
         email = input("Entre o seu email: ")
         password = input("Entre a sua senha: ")
 
-        tok = login(email, password)
-
-        if tok != False:
+        if login(email, password):
             logged = True
 
     while True:
@@ -92,13 +118,13 @@ def main():
         elif tokens[0] == "notificacoes":
             notifications()
         elif tokens[0] == "fazer-exame":
-            if len(tokens) != 2: 
+            if len(tokens) != 2:
                 print("Comando invalido")
                 continue
             exam_id = tokens[1]
             do_exam(exam_id)
         elif tokens[0] == "verificar-exame":
-            if len(tokens) != 2: 
+            if len(tokens) != 2:
                 print("Comando invalido")
                 continue
             exam_id = tokens[1]
@@ -107,6 +133,5 @@ def main():
             print("Comando invalido")
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
