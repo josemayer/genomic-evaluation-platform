c3b8c90ee440a5f2f6341fe3f866d6e35502d5ee
Maximilian Cabrajac Göritz <mcgoeritz@gmail.com>
2023-07-10 18:19:03 -0300

    Add TODOS end point

--- src/app/controllers/notifications.js
+++ src/app/controllers/notifications.js
@@ -13,7 +13,7 @@ async function listUserNotifications(req, res) {
   }
 
   try {
-    const user = await auth.decodeToken(auth_header);
+    const user = auth.decodeToken(auth_header);
     const userNotifications = await notifications.getNotifications(user.userData.id);
 
     res.status(200);
@@ -27,6 +27,35 @@ async function listUserNotifications(req, res) {
   return res;
 }
 
+async function listUserToDos(req, res) {
+  const auth_header = req.headers.authorization;
+
+  if (!auth_header) {
+    res.status(400);
+    res.json({
+      message: 'Required authorization header',
+    });
+    return res;
+  }
+
+
+  try {
+    const user = auth.decodeToken(auth_header);
+    const userTodos = await notifications.getToDos(user.userData.types);
+
+    res.status(200);
+    res.json(userTodos);
+  } catch (err) {
+    res.status(err.status_code || 500);
+    console.log(err)
+    res.json({
+      message: err.message
+    });
+  }
+  return res;
+}
+
 module.exports = {
   listUserNotifications,
+  listUserToDos,
 };
