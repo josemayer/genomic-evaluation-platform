5821c38263a612213d9443bf19438cc8d42dad3a
Luã Santilli <lua.til@usp.br>
2023-07-13 20:42:52 -0300

    start: Check dependencies on ./start script
    Signed-off-by: Luã Santilli <lua.til@usp.br>

--- start.sh
+++ start.sh
@@ -1,10 +1,46 @@
 #!/bin/bash
 
-# Create .env files from the simple .env.sample files
-for f in $(find . -name .env.sample); do cp $f $(echo $f | sed 's/.sample//g'); done
+# Check dependencies
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Error: docker is not installed.' >&2
+  exit 1
+fi
 
-# Delete all .env files 
-# rm $(find . -name .env)
+if ! [ -x "$(command -v make)" ]; then
+  echo 'Error: make is not installed.' >&2
+  exit 1
+fi
 
+if ! [ -x "$(command -v python3)" ]; then
+  echo 'Error: python3 is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v g++)" ]; then
+  echo 'Error: g++ is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v find)" ]; then
+  echo 'Error: find is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v sed)" ]; then
+  echo 'Error: sed is not installed.' >&2
+  exit 1
+fi
+
+# Create .env files from the sample .env.sample files
+for f in $(find . -name .env.sample); 
+    do cp $f $(echo $f | sed 's/.sample//g'); 
+done
+
+# Generate the world
+pushd world-generator/
+make
+popd
+
+# Run docker compose
 docker compose --profile dev build
 docker compose --profile dev up
